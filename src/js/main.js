68ac4944ca3482f499e3e097742aadb2c9521717
GhostMan11 <ghost.man4477@gmail.com>
2019-06-07 01:16:48 +0400

    FullPage JS

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Section I', 'Section II', 'Section III'],
+    showActiveTooltip: false,
+    scrollingSpeed: 1100,
+    controlArrows: true,
+    slidesNavigation: true,
+
+    anchors: ['section1', 'section2', 'section3']
+})
